a0e52a73435ca232bfe6f4729c0b8d7acabbb3dd
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2017-10-06 23:49:04 +0200

    add index page fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-export default () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import fetch from 'isomorphic-unfetch'
+
+export default class Index extends React.Component {
+  static async getInitialProps () {
+    const response = await fetch('http://localhost:4000/users')
+    const json = await response.json()
+
+    return { users: json.data }
+  }
+
+  render () {
+    const { users } = this.props
+
+    return (
+      <div>
+        {users.map(user => {
+          return <p>{user.title}</p>
+        })}
+        <p>Hello world</p>
+      </div>
+    )
+  }
+}
